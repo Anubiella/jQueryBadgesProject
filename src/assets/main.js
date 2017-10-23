83b011458b4fd8475e26a68b853c163c4a558ad4
Anubiella <rathvelcar@libero.it>
2017-10-23 22:50:45 +0200

    Project complete

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,14 @@
-$(function() {
+var Badges = {};
 
-  // your code will go here
-
+$(document).ready(function() {
+	$.ajax({
+	    url: 'https://www.codeschool.com/users/Anubiella.json',
+	    dataType: 'jsonp',
+	    success: function(response) {	      
+	      Badges = (response.courses.completed);
+	      for (var i=0;i<Badges.length;i++){
+	      	$('#badges').append('<div class="course"><h3>'+Badges[i].title+'</h3><img src='+Badges[i].badge+'><a href='+Badges[i].url+' target="_blank" class="btn btn-primary">See Course</a></div>');
+	      }	
+	    }
+	});
 });
